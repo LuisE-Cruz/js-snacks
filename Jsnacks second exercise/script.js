153c2b78aa79908e7478b26b776750e8bce0c0bc
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-04 12:38:45 +0100

    Consegna secondo esercizio

---
+++ Jsnacks second exercise/script.js
@@ -0,0 +1,14 @@
+/*
+JSnack 3
+ Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+ Il programma stampa la somma di tutti i numeri inseriti. 
+ Esegui questo programma in due versioni, con il for e con il while (facoltativo)
+ */
+
+let result = 0
+
+for (let i = 0; i < 5; i++) {
+    const addNumber = parseInt(prompt('Inserisci un numero'));
+    result += addNumber;
+}
+console.log("Il risultato di tutti i numeri sommati è: " + result)
